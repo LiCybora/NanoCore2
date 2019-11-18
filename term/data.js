478966a45a6754d3efb9235deb59794a44299b06
LiCybora <40160522+LiCybora@users.noreply.github.com>
2019-11-18 21:32:51 +0800

    push version update

--- term/data.js
+++ term/data.js
@@ -30,7 +30,7 @@ const assert = require("assert");
 
 // ----------------------------------------------------------------------------------------------------------------- //
 
-exports.version = "1.0.0.130";
+exports.version = "1.0.0.131";
 
 exports.based_on = [
     "uBlock Origin",
